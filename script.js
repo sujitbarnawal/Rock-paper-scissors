73fb86a749fa43f0c0a063dfcf6357629afcadc4
sujitbarnawal <166618250+sujitbarnawal@users.noreply.github.com>
2024-05-18 13:39:16 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let rock = document.querySelector(".img_rock")
 let paper = document.querySelector(".img_paper")
 let scissor = document.querySelector(".img_scissor")
-
+let footer = document.querySelector(".footer")
 
 
 
@@ -9,15 +9,15 @@ rock.addEventListener("click", () => {
     let arr = ["rock", "paper", "scissor"]
     let a = Math.floor(Math.random() * 3)
     let computer_choice = arr[a]
-    if(computer_choice=="rock"){
-        alert("Draw")
+    if (computer_choice == "rock") {
+        footer.innerText = "Game is draw"
     }
-    else if(computer_choice=="paper"){
-        alert("You Lost")
+    else if (computer_choice == "paper") {
+        footer.innerText = "Paper beats Rock. You lost"
     }
 
-    else{
-        alert("You win")
+    else {
+        footer.innerText = "Rock beats Scissor. You won"
     }
 })
 
@@ -25,30 +25,30 @@ paper.addEventListener("click", () => {
     let arr = ["rock", "paper", "scissor"]
     let a = Math.floor(Math.random() * 3)
     let computer_choice = arr[a]
-    if(computer_choice=="paper"){
-        alert("Draw")
+    console.log(computer_choice)
+    if (computer_choice == "paper") {
+        footer.innerText = "Game is draw"
     }
-    else if(computer_choice=="scissor"){
-        alert("You Lost")
+    else if (computer_choice == "scissor") {
+        footer.innerText="Scissor beats paper. You lost"
     }
 
-    else{
-        alert("You win")
-    }
+    else {
+        footer.innerText="Paper beats Rock. You won"  }
 })
 
 scissor.addEventListener("click", () => {
     let arr = ["rock", "paper", "scissor"]
     let a = Math.floor(Math.random() * 3)
     let computer_choice = arr[a]
-    if(computer_choice=="scissor"){
-        alert("Draw")
+    if (computer_choice == "scissor") {
+        footer.innerText = "Game is draw"
     }
-    else if(computer_choice=="rock"){
-        alert("You Lost")
+    else if (computer_choice == "rock") {
+        footer.innerText="Rock beats Scissor. You lost"
     }
 
-    else{
-        alert("You win")
+    else {
+        footer.innerText="Scissor beats paper. You won"
     }
 })
